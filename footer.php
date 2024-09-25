36eb13753672a27a61029e9d706f7fcce6aeaf68
PANDIT JI <95570961+swaghamirpur@users.noreply.github.com>
2024-09-25 12:41:33 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer>
+	<p class='text-center'>	Copyright &copy;<script>document.write(new Date().getFullYear());</script> -  BBMS
+	</p>
+</footer>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<!-- <script>
+	$(function() {
+
+	$(".DATES").datepicker({ 
+
+dateFormat: "yy-mm-dd",
+changeMonth: true,
+changeYear: true,
+yearRange: '1900:' + new Date().getFullYear()
+ }).val();
+	});
+</script> -->
+
+
+
